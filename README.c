2127181aed81571de52de93a0ff669f8cfd0e966
annapureddyjahnavi123-source <annapureddyjahnavi123@gmail.com>
2025-10-29 10:15:32 +0530

    Create README.c

---
+++ README.c
@@ -0,0 +1,10 @@
+//BIODATA
+#include<stdio.h>
+int main ()
+{
+  printf("Name::A.Jahnavi");
+ptintf("DOB::19-10-2007");
+printf("Place::Proddatur");
+ptintf("Father name::A.Narayanareddy");
+return 0;
+}
