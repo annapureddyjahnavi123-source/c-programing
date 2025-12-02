2fe068180f72b07108a8914cefb53fe533671519
annapureddyjahnavi123-source <annapureddyjahnavi123@gmail.com>
2025-12-02 16:13:11 +0530

    Add files via upload

---
+++ with out. strcat().c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s1[20],s2[20];
+	int l,i;
+	printf("enter string 1 name");
+	scanf("%s",&s1);
+	printf("enter string 2 name");
+	scanf("%s",&s2);
+	for(i=0;s1[i]!='\0';i++)
+	{
+		s1[l++]=s2[i];
+	}
+	s1[l]='\0';
+	printf("merge string=%s",s1);
+	return 0;
+}
