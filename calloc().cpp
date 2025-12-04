155d75abffc4be762b176e62dae7eb6486229032
annapureddyjahnavi123-source <annapureddyjahnavi123@gmail.com>
2025-12-04 11:43:34 +0530

    Add files via upload

---
+++ calloc().cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int *ptr,n,i;
+	printf("enter n value");
+	scanf("%d",&n);
+	ptr=(int*)calloc(n,sizeof(int));
+    printf("enter integer numbers");
+	for(i=0;i<n;i++)
+	scanf("%d",ptr+i);
+	return 0;
+}
+
