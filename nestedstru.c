62a14c8c1a13e83d9ce0ef5d7f375a6ae2fb2bca
annapureddyjahnavi123-source <annapureddyjahnavi123@gmail.com>
2026-05-01 11:17:36 +0530

    Add files via upload

---
+++ nestedstru.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+struct address
+{
+	char street[10];
+	char city[10];
+	long int pcode;
+};
+struct student
+{
+	int rno;
+	char name[10];
+	float per;
+	struct address a;
+};
+int main()
+{
+	struct student s={101,"mahi",98.90,{"aditya","bvrm",534202}};
+	printf("%d %s %f %s %s %d",s.rno,s.name,s.per,s.a.street,s.a.city,s.a.pcode);
+	return 0;
+}
